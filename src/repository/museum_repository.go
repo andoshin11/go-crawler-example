bab98bb90c404b92eb0ac618544e20019b4e682b
andoshin11 <shinglish11@gmail.com>
2018-08-14 15:43:14 +0900

    Refactor repository

--- src/repository/museum_repository.go
+++ src/repository/museum_repository.go
@@ -101,17 +101,3 @@ func (r *museumRepository) UpdateItem(ctx context.Context, identifier string, mu
 
 	return
 }
-
-// func StructToMap(data interface{}) map[string]interface{} {
-// 	result := make(map[string]interface{})
-// 	elem := reflect.ValueOf(data).Elem()
-// 	size := elem.NumField()
-
-// 	for i := 0; i < size; i++ {
-// 		field := elem.Type().Field(i).Name
-// 		value := elem.Field(i).Interface()
-// 		result[field] = value
-// 	}
-
-// 	return result
-// }
